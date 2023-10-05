8cb396881dad41c4144612b4483322cb0036e2ae
manighahrmani <mashapicasso@gmail.com>
2023-10-05 19:15:03 +0100

    Remove unused examples

--- pract5.py
+++ pract5.py
@@ -1,32 +1,27 @@
 from graphics import *
 import math
 
-# `product` function that has two parameters `a` and `b` and returns their product
+
+def greet(name):
+    name = input("What is your name? ")
+    return f"Hello, {name}!"
+
+
 def product(a, b):
     return a * b
 
 
-# `divide` function that has two parameters `a` and `b` and returns their quotient
-def divide(a, b):
-    return a / b
-
-
-# `main` function that takes two numbers and outputs their product and quotient
 def main():
+    myName = input("What is your name? ")
+    greeting = greet(myName)
+    print(greeting)
+
     num1 = int(input("Enter a number: "))
     num2 = int(input("Enter another number: "))
     productResult = product(num1, num2)
-    divideResult = divide(num1, num2)
-    print("{0} times {1} is {2}.".format(
-        num1, num2, productResult))
-    print("{0} divided by {1} is {2}.".format(
-        num1, num2, divideResult))
+    print(f"{num1} x {num2} = {productResult}")
 
-# `greet` function that has a parameter `name` and returns a greeting
-def greet(name):
-    return "Hello, {0}!".format(name)
 
-# `calcFutureValue` function has two parameters `amount` and `years`, returns the future value of the investment
 def calcFutureValue(amount, years):
     interestRate = 0.065
     for year in range(years):
@@ -34,13 +29,13 @@ def calcFutureValue(amount, years):
     return amount
 
 
-# `futureValue` function that takes an amount and number of years and outputs the future value of the investment
 def futureValue():
     amount = float(input("Enter an amount to invest: "))
     years = int(input("Enter the number of years: "))
     finalValue = calcFutureValue(amount, years)
     print("Investing GBP {0:0.2f} for {1} years".format(amount, years),
           "will result in GBP {0:0.2f}.".format(finalValue))
+
 
 # For exercises 1 and 2
 def areaOfCircle(radius):
